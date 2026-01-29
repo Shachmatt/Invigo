a227f0b3a24adcfcbd6824d170cbd9f7d74428b6
Shachmatt <125585844+Shachmatt@users.noreply.github.com>
2026-01-29 14:50:21 +0100

    zrušil jsem chybu s reloadováním

--- src/assets/bottom.jsx
+++ src/assets/bottom.jsx
@@ -15,8 +15,14 @@ export default function Bottom({ shoutout, button, disabled, link, onClicked}) {
 
 <div className="summary">
             <div className="xp-award"></div>
-            {link===1 && <a href="/"><button disabled={disabled} className="completion-badge">{button}</button></a>}
-            {link===0 &&<button disabled={disabled} onClick={onClicked} className="completion-badge">{button}</button>}
+{link === 1 && (
+  <a href="#" onClick={(e) => { 
+    e.preventDefault(); // Zabrání přidání # do URL
+    window.location.reload(); 
+  }}>
+    <button disabled={disabled} className="completion-badge">{button}</button>
+  </a>
+)}            {link===0 &&<button disabled={disabled} onClick={onClicked} className="completion-badge">{button}</button>}
         </div>  
 
     )
